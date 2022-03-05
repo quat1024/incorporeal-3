44f6ab10de08f5612c2f6bb9942bc276b0927c1c
quat1024 <quat1024@users.noreply.github.com>
2022-03-05 05:29:02 -0500

    get trolled by mixin

--- Common/src/main/java/agency/highlysuspect/incorporeal/mixin/ModTilesMixin.java
+++ Common/src/main/java/agency/highlysuspect/incorporeal/mixin/ModTilesMixin.java
@@ -4,8 +4,6 @@ import agency.highlysuspect.incorporeal.block.IncBlocks;
 import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import vazkii.botania.common.block.ModBlocks;
 import vazkii.botania.common.block.tile.ModTiles;
@@ -20,7 +18,8 @@ public class ModTilesMixin {
 	@ModifyVariable(
 		method = "type",
 		at = @At("HEAD"),
-		argsOnly = true
+		argsOnly = true,
+		remap = false
 	)
 	private static Block[] moreTaters(Block... blocks) {
 		if(blocks.length != 0 && blocks[0] == ModBlocks.tinyPotato) {
