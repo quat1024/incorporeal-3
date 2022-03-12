133af1e6089206643c01bcd89e373bc6d73fd7e3
quat1024 <quat1024@users.noreply.github.com>
2022-03-11 23:24:57 -0500

    Revert "impl ender soul core masking w/ Crafting Patterns"
    lol use the red string constrictor instead

--- Fabric/src/main/java/agency/highlysuspect/incorporeal/platform/fabric/EnderSoulCoreStorage.java
+++ Fabric/src/main/java/agency/highlysuspect/incorporeal/platform/fabric/EnderSoulCoreStorage.java
@@ -4,19 +4,14 @@ import agency.highlysuspect.incorporeal.block.entity.EnderSoulCoreBlockEntity;
 import com.google.common.primitives.Ints;
 import net.fabricmc.fabric.api.transfer.v1.item.InventoryStorage;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
-import net.fabricmc.fabric.api.transfer.v1.item.base.SingleStackStorage;
 import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
 import net.fabricmc.fabric.api.transfer.v1.storage.StorageView;
-import net.fabricmc.fabric.api.transfer.v1.storage.base.CombinedStorage;
 import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
 import net.minecraft.core.Direction;
 import net.minecraft.world.inventory.PlayerEnderChestContainer;
-import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 @SuppressWarnings("UnstableApiUsage")
 public record EnderSoulCoreStorage(EnderSoulCoreBlockEntity be) implements Storage<ItemVariant> {
@@ -24,17 +19,10 @@ public record EnderSoulCoreStorage(EnderSoulCoreBlockEntity be) implements Stora
 		this(entity);
 	}
 	
-	//TODO: maybe cache this for one tick, or something
 	private Storage<ItemVariant> getDelegate() {
 		PlayerEnderChestContainer enderChest = be.getEnderChest();
 		if(enderChest == null) return Storage.empty();
-		else if(be.isCompletelyUnmasked()) return InventoryStorage.of(enderChest, null);
-		
-		List<Storage<ItemVariant>> maskedList = new ArrayList<>();
-		for(int i = 0; i < enderChest.getContainerSize(); i++) {
-			if(be.canAccessSlot(i)) maskedList.add(new YourApiSucks(enderChest, i));
-		}
-		return new CombinedStorage<>(maskedList);
+		else return InventoryStorage.of(enderChest, null);
 	}
 	
 	@Override
@@ -129,68 +117,5 @@ public record EnderSoulCoreStorage(EnderSoulCoreBlockEntity be) implements Stora
 	@Override
 	public long simulateExtract(ItemVariant resource, long maxAmount, @Nullable TransactionContext transaction) {
 		return getDelegate().simulateExtract(resource, maxAmount, transaction);
-	}
-	
-	//Fucking reimplementation of InventorySlotWrapper because its private in fabric transfer api and bound too closely to InventoryStorageImpl
-	public static class YourApiSucks extends SingleStackStorage {
-		private final PlayerEnderChestContainer pissOff;
-		final int slot;
-		private ItemStack whoCares = null;
-		
-		public YourApiSucks(PlayerEnderChestContainer pissOff, int slot) {
-			this.pissOff = pissOff;
-			this.slot = slot;
-		}
-		
-		@Override
-		protected ItemStack getStack() {
-			return pissOff.getItem(slot);
-		}
-		
-		@Override
-		protected void setStack(ItemStack stack) {
-			pissOff.setItem(slot, stack);
-		}
-		
-		@Override
-		protected boolean canInsert(ItemVariant itemVariant) {
-			return pissOff.canPlaceItem(slot, itemVariant.toStack());
-		}
-		
-		@Override
-		protected int getCapacity(ItemVariant itemVariant) {
-			return Math.min(pissOff.getMaxStackSize(), itemVariant.getItem().getMaxStackSize());
-		}
-		
-		@Override
-		public void updateSnapshots(TransactionContext transaction) {
-			//The real one tries to be more clever about this but i dont give a shit
-			//Fuck off
-			pissOff.setChanged();
-			super.updateSnapshots(transaction);
-		}
-		
-		@Override
-		protected void releaseSnapshot(ItemStack snapshot) {
-			whoCares = snapshot;
-		}
-		
-		//coy pasting
-		@Override
-		protected void onFinalCommit() {
-			// Try to apply the change to the original stack
-			ItemStack original = whoCares;
-			ItemStack currentStack = getStack();
-			
-			if (!original.isEmpty() && original.getItem() == currentStack.getItem()) {
-				// None is empty and the items match: just update the amount and NBT, and reuse the original stack.
-				original.setCount(currentStack.getCount());
-				original.setTag(currentStack.hasTag() ? currentStack.getTag().copy() : null);
-				setStack(original);
-			} else {
-				// Otherwise assume everything was taken from original so empty it.
-				original.setCount(0);
-			}
-		}
 	}
 }
--- Forge/src/main/java/agency/highlysuspect/incorporeal/platform/forge/EnderSoulCoreItemHandler.java
+++ Forge/src/main/java/agency/highlysuspect/incorporeal/platform/forge/EnderSoulCoreItemHandler.java
@@ -23,15 +23,12 @@ public record EnderSoulCoreItemHandler(EnderSoulCoreBlockEntity be) implements I
 	@NotNull
 	@Override
 	public ItemStack getStackInSlot(int slot) {
-		if(!be.canAccessSlot(slot)) return ItemStack.EMPTY;
-		else return getDelegate().getStackInSlot(slot);
+		return getDelegate().getStackInSlot(slot);
 	}
 	
 	@NotNull
 	@Override
 	public ItemStack insertItem(int slot, @NotNull ItemStack stack, boolean simulate) {
-		if(!be.canAccessSlot(slot)) return stack; //no u
-		
 		ItemStack leftover = getDelegate().insertItem(slot, stack, simulate);
 		if(!simulate) be.trackItemMovement(stack.getCount() - leftover.getCount());
 		return leftover;
@@ -40,8 +37,6 @@ public record EnderSoulCoreItemHandler(EnderSoulCoreBlockEntity be) implements I
 	@NotNull
 	@Override
 	public ItemStack extractItem(int slot, int amount, boolean simulate) {
-		if(!be.canAccessSlot(slot)) return ItemStack.EMPTY;
-		
 		ItemStack extracted = getDelegate().extractItem(slot, amount, simulate);
 		if(!simulate) be.trackItemMovement(extracted.getCount());
 		return extracted;
@@ -49,13 +44,11 @@ public record EnderSoulCoreItemHandler(EnderSoulCoreBlockEntity be) implements I
 	
 	@Override
 	public int getSlotLimit(int slot) {
-		if(!be.canAccessSlot(slot)) return 0;
-		else return getDelegate().getSlotLimit(slot);
+		return getDelegate().getSlotLimit(slot);
 	}
 	
 	@Override
 	public boolean isItemValid(int slot, @NotNull ItemStack stack) {
-		if(!be.canAccessSlot(slot)) return false;
-		else return getDelegate().isItemValid(slot, stack);
+		return getDelegate().isItemValid(slot, stack);
 	}
 }
