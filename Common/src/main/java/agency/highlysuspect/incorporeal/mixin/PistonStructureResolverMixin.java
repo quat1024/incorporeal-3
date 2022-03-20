84c9b0f851c46b712496b391d2d1de80e16f6012
quat1024 <quat1024@users.noreply.github.com>
2022-03-20 07:44:14 -0400

    make them pushable as a unit if you have a movable block entity mod i guess

---
+++ Common/src/main/java/agency/highlysuspect/incorporeal/mixin/PistonStructureResolverMixin.java
@@ -0,0 +1,22 @@
+package agency.highlysuspect.incorporeal.mixin;
+
+import agency.highlysuspect.incorporeal.block.IncBlocks;
+import net.minecraft.world.level.block.piston.PistonStructureResolver;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PistonStructureResolver.class)
+public class PistonStructureResolverMixin {
+	@Inject(method = "isSticky", at = @At("HEAD"), cancellable = true)
+	private static void isStickyHook(BlockState state, CallbackInfoReturnable<Boolean> cir) {
+		if(state.getBlock() == IncBlocks.POINTED_DATASTONE) cir.setReturnValue(true);
+	}
+	
+	@Inject(method = "canStickToEachOther", at = @At("HEAD"), cancellable = true)
+	private static void canStickToEachOtherHook(BlockState a, BlockState b, CallbackInfoReturnable<Boolean> cir) {
+		if(a.getBlock() == IncBlocks.POINTED_DATASTONE && b.getBlock() == IncBlocks.POINTED_DATASTONE) cir.setReturnValue(true);
+	}
+}
