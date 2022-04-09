96835b22ea7d28d4960216ccd1e499540aed3af6
quat1024 <quat1024@users.noreply.github.com>
2022-04-09 06:40:59 -0400

    Also allow throwing them like real ender pearls

--- Common/src/main/java/agency/highlysuspect/incorporeal/item/BoundEnderPearlItem.java
+++ Common/src/main/java/agency/highlysuspect/incorporeal/item/BoundEnderPearlItem.java
@@ -7,7 +7,10 @@ import net.minecraft.core.Position;
 import net.minecraft.core.dispenser.DispenseItemBehavior;
 import net.minecraft.core.dispenser.OptionalDispenseItemBehavior;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -43,7 +46,9 @@ public class BoundEnderPearlItem extends Item {
 	public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
 		ItemStack held = player.getItemInHand(hand);
 		
-		if(getOwnerUuid(held) == null) {
+		UUID ownerUuid = getOwnerUuid(held);
+		if(ownerUuid == null) {
+			//Unbound. Bind it to the player
 			ItemStack ya;
 			if(player.getAbilities().instabuild) {
 				ya = held.copy();
@@ -58,7 +63,27 @@ public class BoundEnderPearlItem extends Item {
 			return InteractionResultHolder.success(held);
 		}
 		
-		return InteractionResultHolder.pass(held);
+		//Already bound; act like a working enderpearl that teleports the bound player
+		
+		level.playSound(player, player.getX(), player.getY(), player.getZ(), SoundEvents.ENDER_PEARL_THROW, SoundSource.NEUTRAL, .5f, .4f / (level.getRandom().nextFloat() * .4f + .8f));
+		
+		if(level instanceof ServerLevel slevel) {
+			ServerPlayer owner = slevel.getServer().getPlayerList().getPlayer(ownerUuid);
+			if(owner == null || owner.level != level) {
+				return InteractionResultHolder.fail(held);
+			}
+			
+			//basically paste from EnderpearlItem
+			//note that owner does't necessarily equal "player", the thrower
+			ThrownEnderpearl pearl = new ThrownEnderpearl(slevel, owner);
+			pearl.shootFromRotation(player, player.getXRot(), player.getYRot(), 0, 1.5f, 1);
+			pearl.setItem(held);
+			slevel.addFreshEntity(pearl);
+			
+			if(!player.getAbilities().instabuild) held.shrink(1);
+		}
+		
+		return InteractionResultHolder.success(held);
 	}
 	
 	@Override
@@ -69,14 +94,16 @@ public class BoundEnderPearlItem extends Item {
 	public class DispenseBehavior extends OptionalDispenseItemBehavior {
 		@Override
 		protected ItemStack execute(BlockSource source, ItemStack stack) {
+			ServerLevel level = source.getLevel();
+			
 			UUID ownerUuid = getOwnerUuid(stack);
 			if(ownerUuid == null) {
 				setSuccess(false);
 				return stack;
 			}
 			
-			ServerPlayer player = source.getLevel().getServer().getPlayerList().getPlayer(ownerUuid);
-			if(player == null) {
+			ServerPlayer owner = level.getServer().getPlayerList().getPlayer(ownerUuid);
+			if(owner == null || owner.level != level) {
 				setSuccess(false);
 				return stack;
 			}
@@ -85,12 +112,12 @@ public class BoundEnderPearlItem extends Item {
 			Position pos = DispenserBlock.getDispensePosition(source);
 			Direction dir = source.getBlockState().getValue(DispenserBlock.FACING);
 			
-			ThrownEnderpearl pearl = new ThrownEnderpearl(source.getLevel(), player);
+			ThrownEnderpearl pearl = new ThrownEnderpearl(level, owner);
 			//move it to the dispenser's dispense position instead of the position of the player
 			pearl.setPos(pos.x(), pos.y(), pos.z());
 			pearl.shoot(dir.getStepX(), dir.getStepY() + 0.1f, dir.getStepZ(), 1.1f, 6f);
 			pearl.setItem(stack);
-			source.getLevel().addFreshEntity(pearl);
+			level.addFreshEntity(pearl);
 			
 			stack.shrink(1);
 			setSuccess(true);
